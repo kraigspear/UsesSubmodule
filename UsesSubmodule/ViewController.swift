a1d23be520a581757e031abab60b18be6ece2d3b
Kraig Spear <kraigspear@gmail.com>
2020-08-28 05:15:28 -0400

    Initial Commit

---
+++ UsesSubmodule/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UsesSubmodule
+//
+//  Created by Kraig Spear on 8/28/20.
+//  Copyright © 2020 DICKSSportingGoods. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
